e9a7ce063e7d5d640cf3c7cb1fbf5eb001b10cb2
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2021-04-12 22:17:08 -0500

    table code

--- data/chartable.js
+++ data/chartable.js
@@ -2,19 +2,37 @@ const charTable = document.querySelector('.smp-char-table')
 const tRows = [...charTable.rows];
 const tableHeaders = [...tRows.shift().cells]
 
-const getColumnName = (head) => {
-	const txt = head.innerText
-	return txt
+//* col names
+const createRecordProperties = (headerCells) => {
+	const propNames = headerCells.map(h => {
+		const n = h.innerText.trim().toLowerCase()
+		// const n = getColumnName(h).trim().toLowerCase()
+		return n
+	})
+	propNames.unshift('id')
+	propNames.splice(2, 0, 'imgsrc')
+	console.log(propNames);
+	return propNames;
 }
 
-const cnArray = tableHeaders.map(h => {
-	const n = getColumnName(h).trim().toLowerCase()
-	return n
-})
-console.log(cnArray);
+createRecordProperties(tableHeaders);
+//* end col names
 
+const rowData = [...tRows]
+	.map((r, i) => {
+		const cellData = [...r.cells]
+			.map(c => {
+				if (c.children.length >= 2) {
+					let charName = c.children[1].innerText
+					let imgsrc = c.children[0].src
+					return [charName, imgsrc]
+				} else {
+					return [c.innerText];
+				}
+			}).flat();
+		return [++i].concat(cellData)
+	}).reduce((sum, curr) => {
+		console.log(curr);
+	}, {})
 
-[...tRows].forEach(r => {
-	
-	console.log(r.cells[0].children);
-})
+console.log(rowData);
