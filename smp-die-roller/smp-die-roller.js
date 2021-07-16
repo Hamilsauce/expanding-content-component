52c8d35a0a57aba35f68511dc5d9d7a96d667ea7
jacob126 <jacob@theleaguestock.com>
2021-07-15 17:23:31 -0700

    chart yeah

--- smp-die-roller/smp-die-roller.js
+++ smp-die-roller/smp-die-roller.js
@@ -5,16 +5,6 @@ let selectedOption;
 let selectedChar;
 const charSelect = ham.qs('#character-select')
 
-const charSelectTemplate = `
-	<select name="character-select" class="character-select" id="character-select">
-		<option value="wario" class="character-select-option">Wario</option>
-		<option value="boo" class="character-select-option">Boo</option>
-	</select>`;
-
-const charSelectOptionTemplate = `
-		<option value="wario" class="character-select-option">Wario</option>
-	`;
-
 const createSelectOptions = chars => {
 	const optionElements = chars
 		.map((char, i) => {
@@ -47,8 +37,8 @@ const updateCharDisplay = char => {
 	const nameEl = ham.qs('.character-name');
 	const imgEl = ham.qs('.character-image');
 	const dieContainter = ham.qs('.character-die-container');
-
 	const dieSideElements = char.die.map((side, i) => generateDieSideElement(side, i));
+
 	imgEl.src = char.imgsrc;
 	nameEl.textContent = char.name;
 
@@ -58,25 +48,14 @@ const updateCharDisplay = char => {
 }
 
 const rollDie = dieSides => {
-	console.log(dieSides);
 	const getRandomNumber = (min, max) => {
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-
-	// this.game.toggleRollClasses(die);
 	let dieSideIndex = getRandomNumber(0, 5);
-	console.log('rando', dieSideIndex);
-	console.log('dieindex',dieSides[dieSideIndex]);
 	return dieSides[dieSideIndex]
-
-
 }
-
-
-
-
 
 fetch('../data/character-data.json')
 	.then(response => response.json())
@@ -85,12 +64,8 @@ fetch('../data/character-data.json')
 		createSelectOptions(charData)
 	});
 
-
-
-
 charSelect.addEventListener('change', e => {
 	selectedOption = e.target.selectedOptions[0]
-
 	selectedChar = charData.find(_ => _.id === +selectedOption.dataset.id)
 	const evt = new CustomEvent('charSelectionChange', {
 		bubbles: true,
@@ -101,17 +76,68 @@ charSelect.addEventListener('change', e => {
 	e.target.dispatchEvent(evt);
 });
 
-
-
 ham.qs('.app').addEventListener('charSelectionChange', e => {
 	updateCharDisplay(e.detail.char)
-	
-
 })
 
 ham.qs('.roll-submit-button').addEventListener('click', e => {
 	const roll = rollDie(selectedChar.die)
-ham.qs('.roll-result').textContent = roll
-
-	
+	chart('bar', selectedChar.die)
+	ham.qs('.roll-result').textContent = roll
 })
+
+const chart = (chartType = 'bar', die) => {
+	const ctx = ham.qs('canvas').getContext('2d');
+	const chart = new Chart(ctx, {
+		type: chartType,
+		data: {
+			labels: die,
+			datasets: [{
+				label: 'Totals',
+				//TODO replace w dynamic data
+				data: [1, 2, 3, 4, 5, 6],
+				backgroundColor: ['#473876 ', '#AC4F46', '#358246', '#AC9A46', '#287670', '#A5754A'],
+				borderColor: ['#ffffffe0 ', '#ffffffe0', '#ffffffe0', '#ffffffe0', '#ffffffe0', '#ffffffe0'],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			legend: {
+				display: false,
+				position: 'bottom',
+				labels: {
+					boxWidth: 0,
+					boxHeight: 0,
+					color: '#ffffff00'
+				}
+			},
+			scales: {
+				xAxes: [{
+					display: true,
+					scaleLabel: {
+						display: true,
+						labelString: 'Die Side Value',
+						fontColor: '#ffffff',
+						fontSize: 12
+					},
+					ticks: {
+						fontColor: '#ffffff',
+						fontSize: 12
+					},gridLines: {
+						color: '#ffffff00'
+					},
+				}],
+				yAxes: [{
+					gridLines: {
+						color: '#B9C7B090'
+					},
+						ticks: {
+						fontColor: '#ffffff',
+						stepSize: 1,
+						fontSize: 14
+					}
+				}]
+			},
+		}
+	});
+}
